921cec77cfc1501dc4a468c47c26a99f86453615
Nathan Edwards <nwesoccer@gmail.com>
2014-10-10 01:04:50 -0400

    Moved default task

--- gulp_tasks/default.js
+++ gulp_tasks/default.js
@@ -1,8 +1,5 @@
-var requireDir = require('require-dir');
 var gulp = require('gulp');
 
 gulp.task('default', function() {
     console.log('default task')
 });
-
-requireDir('./gulp_tasks', { recurse: true });
